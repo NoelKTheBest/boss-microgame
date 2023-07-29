e0a7a557b6c7d5d7eaf1efca395e188f9abf2a7d
Noel K <nk.toptier.0@gmail.com>
2023-07-29 14:42:26 -0700

    Added in tilesets and new animations

---
+++ Assets/Scripts/Editor/LoadSprites.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LoadSprites : Editor
+{
+    AnimationClipCurveData data = new AnimationClipCurveData();
+    Animation animation;
+    AnimationCurve curve;
+    //AnimationUtility
+
+    private void OnEnable()
+    {
+        //curve.keys.GetValue(0);
+        //data.curve.keys.GetValue(0);
+        
+    }
+}
